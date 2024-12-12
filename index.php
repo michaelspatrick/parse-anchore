9e92abfa020d1e3d27a47058a1d1590c8566123e
Mike Patrick <45206934+michaelspatrick@users.noreply.github.com>
2024-12-12 13:14:17 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<form action="parse-web.php" method="post" enctype="multipart/form-data">
+  <input name="infile" id="infile" type="file">
+  <input type="submit" value="Upload" name="submit" id="submit">
+</form>
